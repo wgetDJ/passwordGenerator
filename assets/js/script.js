40e86f7dac639aaf5ded36fed8bf2ba3cc190eb0
wgetdj <dibyajyotimishraiiit@gmail.com>
2021-06-02 12:22:54 +0530

    Generating passwords TODO: fixing the checkbox

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,50 +17,65 @@ const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 const specialCharacters = [" ", "!", "'", '"', "(", ")", "*", "+", ",", "-", "_", ".", "/", ":", ";", "<", ">", "=", "?", "[", "]", "\\", "^", "`", "~", "{", "}"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-const allArrays = [upperCase, lowerCase, specialCharacters, numbers];
+const allCharacters = [upperCase, lowerCase, specialCharacters, numbers];
 
 const btn = document.querySelector("#btn");
 const copy = document.querySelector("#copy");
 const placeHolder = document.querySelector("#placeHolder");
 const yourPassword = document.querySelector("#yourPassword");
 const message = document.querySelector("#msg");
-const uCase = document.querySelector("#upperCase");
-const lCase = document.querySelector("#lowerCase");
-const nums = document.querySelector("#numbers");
-const splChar = document.querySelector("#specialCharacters");
+const uCase = document.querySelector("#upperCase").value;
+const lCase = document.querySelector("#lowerCase").value;
+const nums = document.querySelector("#numbers").value;
+const splChar = document.querySelector("#specialCharacter").value;
 
-const generatePassword = () => {
-    const checkBoxs = [uCase.checked, lCase.checked, nums.checked, splChar.checked];
+const checkBoxValues = [uCase.checked, lCase.checked, nums.checked, splChar.checked];
+
+
+const randomCharacter = (allCharacters, checkBoxValues) => {
+    let character = ""
+    let randomCheckBoxValue = Math.floor(Math.random() * checkBoxValues.length);
+    let availableCharacters = [];
+    for (let i = 0; i < checkBoxValues.length; i++) {
+        if (checkBoxValues[i] != false) {
+            availableCharacters.push(allCharacters[i]);
+        }
+    }
+    let selectedArray = allCharacters[randomCheckBoxValue];
+    character = selectedArray[Math.floor(Math.random() * selectedArray.length)];
+    return character;
+    
+    // if (checkBoxValues[randomCheckBoxValue] == false) {
+    //     randomCharacter(allCharacters, checkBoxValues);
+    // }
+    // else {
+    //     let selectedArray = allCharacters[randomCheckBoxValue];
+    //     character = selectedArray[Math.floor(Math.random() * selectedArray.length)];
+    //     return character;
+    // }
+}
+
+const generatePassword = (checkBoxValues) => {
     let unchecked = 0;
-
-// checking the number of unchecked boxes
-    for (let checks of checkBoxs) {
+    for (let checks of checkBoxValues) {
         if(checks == false) {
             unchecked++;
         }
     }
 
     if (unchecked > 2) {
-       message.textContent = "Plese select at least two!" 
+       message.textContent = "Plese select at least two!";
+       message.style.color = "#ff0000";
     }
     else {
         let pswd = "";
         for (let i = 0; i < 20; i++) {
-            pswd += password(checkBoxs);
+            pswd += randomCharacter(allCharacters, checkBoxValues);
         }
         yourPassword.textContent = pswd;
         placeHolder.style.display = "none";
-
-    }
-}
-
-const password = (checkBoxs) => {
-    checkBoxValue = checkBoxs[Math.floor(Math.random() * checkBoxs.length)];
-    if (checkBoxValue == false) {
-        password(checkBoxs);
-    }
-    else {
-        chracter = checkBoxs[checkBoxValue][Math.floor(Math.random() * checkBoxs[checkBoxValue].length)];
+        copy.style.color = "#eeeeee";
+        copy.title = "Copy";
     }
 }
 
@@ -69,7 +84,8 @@ const copyPassword = () => {
     let cb = navigator.clipboard;
     cb.writeText(passwordToCopy.innerText);
     copy.style.color = "#15e638";
+    copy.title = "Copied";
 }
 
-btn.addEventListener("click", generatePassword);
+btn.addEventListener("click", function(){generatePassword(checkBoxValues)}, false);
 copy.addEventListener("click", copyPassword);
